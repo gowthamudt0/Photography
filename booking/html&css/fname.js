4ab03683525db84bbffae7f7e6cbea413f761ca9
gowthamudt0 <gowthamudt0@gmail.com>
2024-03-12 21:23:15 +0530

    first commit

---
+++ booking/html&css/fname.js
@@ -0,0 +1,7 @@
+function praba()
+{
+var num1=parseInt(prompt("enter your first number"))
+var num2=parseInt(prompt("enter your second number"))
+var x=num1+num2
+}
+document.getElementById("demo1").innerHTML="my full name is:" +x;
